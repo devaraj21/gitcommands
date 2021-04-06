fe6e1b8abd1f93c89bc0d8306192f6498734296c
devaraj21 <komaradevaraj0321@gmail.com>
2021-04-06 22:58:24 +0530

    this is my github

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+
+export default class App extends Component {
+ render() {
+    return(
+      
+     /* <View style={styles.container} >
+       <View style={{flex:1, backgroundColor:'black'}}></View>
+       <View style={{flex:2, backgroundColor:'red'}}></View>
+      </View>*/
+
+    <View style={styles.container} >
+      <View style={{height:80,width:80,backgroundColor:'black'}}>
+        <Text>hiiiii i am Devaraj</Text>
+      </View>
+    </View>
+
+    ); 
+  }
+}
+
+const styles = StyleSheet.create ({
+    container : {
+      flex:1,
+      backgroundColor:'yellow',
+      //flexDirection: 'row',
+      alignItems:'center',
+      justifyContent:'center',
+    }
+});
