d4094891fe9e215f457400efd2e7c11583dec572
Aswini600 <sweetyasu504@gmail.com>
2021-04-07 12:44:12 +0530

    aswini im modified

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ export default class App extends Component {
       <View style={{height:80,width:80,backgroundColor:'black'}}>
         <Text>hiiiii i am Devaraj</Text>
         <Text>hiiiii i am Raamu</Text>
+        <Text>hii im aswini</Text>
       </View>
     </View>
 
