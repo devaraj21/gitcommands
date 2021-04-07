fec3cab095a2680725bfaa2832b5b437a916f4ef
Udayagiri Ramaiah <ramaiahudayagiri@gmail.com>
2021-04-07 11:30:28 +0530

    Ramu modified App.js file

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ export default class App extends Component {
     <View style={styles.container} >
       <View style={{height:80,width:80,backgroundColor:'black'}}>
         <Text>hiiiii i am Devaraj</Text>
+        <Text>hiiiii i am Raamu</Text>
       </View>
     </View>
 
